f094663f292660ac96274c20bfa7c4fc1bd6538d
Chandu Reddy <70282603+chandusreddy@users.noreply.github.com>
2021-02-15 10:00:08 -0800

    Add files via upload
    Initial DSA program files

---
+++ DSA/src/com/chandu/datastructures/arrays/ArrayCheckSum.java
@@ -0,0 +1,43 @@
+package com.chandu.datastructures.arrays;
+
+public class ArrayCheckSum {
+	public static int[] findSum(int[] arr, int n) {
+		int[] result = new int[2]; // to store the pair of values.
+
+		for (int i = 0; i < arr.length; i++) { // traverse arr
+			for (int j = i + 1; j < arr.length; j++) { // traverse arr again
+				if (arr[i] + arr[j] == n) { // find where sum is equal to n
+					result[0] = arr[i];
+					result[1] = arr[j];
+					return result; // return the pair of numbers
+				}
+			}
+		}
+		return arr;
+	}
+
+	public static void main(String args[]) {
+
+		int n = 9;
+		int[] arr = {2,3,5,7,9};
+		if(arr.length>0) {
+		
+		int[] result = findSum(arr,n);
+		int num1 = result[0];
+		int num2 = result[1];
+		if(num1+num2!=n) {
+			System.out.println("Not Found");
+		}
+		else {
+			System.out.println("The Number 1 of the array: " +num1);
+			System.out.println("The Number 2 of the array: " +num2);
+			System.out.println("The sum of the array is: " +n);
+		}
+		
+}
+		else {
+			System.out.println("The Entered array is Empty");
+		}
+	
+	}
+}
