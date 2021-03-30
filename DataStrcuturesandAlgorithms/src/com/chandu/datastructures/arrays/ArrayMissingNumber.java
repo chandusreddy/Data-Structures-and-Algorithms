3ea154a8af40fc78b433c828db762d1293569f55
Chandu0005 <70282603+Chandu0005@users.noreply.github.com>
2021-03-30 11:54:55 -0400

    Array Missing Number

---
+++ DataStrcuturesandAlgorithms/src/com/chandu/datastructures/arrays/ArrayMissingNumber.java
@@ -0,0 +1,32 @@
+package com.chandu.datastructures.arrays;
+
+/**
+ * Solution for finding the missing number in the given Array
+ * 
+ * Find the sum of n number using formula n=n*(n+1)/2 Find the sum of elements
+ * present in given array. Substract (sum of n numbers – sum of elements present
+ * in the array).
+ **/
+public class ArrayMissingNumber {
+
+	private static int findMissingNum(int[] arr) {
+		int n = arr.length + 1;
+		int sum = n * (n + 1) / 2;
+		int resultsum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			resultsum += arr[i];
+		}
+		int missingNum = sum - resultsum;
+		return missingNum;
+	}
+
+	public static void main(String[] args) {
+
+		int[] arr1 = { 5, 1, 4, 2 };
+		System.out.println("Missing number from given array1 is: " + findMissingNum(arr1));
+		int[] arr2 = { 4, 3, 2 };
+		System.out.println("Missing number from given array2 is:" + findMissingNum(arr2));
+
+	}
+
+}
