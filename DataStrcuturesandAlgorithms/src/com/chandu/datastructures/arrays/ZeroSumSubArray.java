3b2988f551d6ae10ffc7fd839e799b43a4f374fd
chandra shekar palle <cshekarpalle@gmail.com>
2025-05-22 12:25:15 -0400

    DSA - Zero Sum SubArray

---
+++ DataStrcuturesandAlgorithms/src/com/chandu/datastructures/arrays/ZeroSumSubArray.java
@@ -0,0 +1,24 @@
+package com.chandu.datastructures.arrays;
+
+import java.util.HashSet;
+
+public class ZeroSumSubArray {
+
+	public boolean zeroSumSubarray(int[] nums) {
+
+		HashSet<Integer> result = new HashSet<Integer>();
+		result.add(0);
+		int currentSum = 0;
+		for (int num : nums) {
+			currentSum += num;
+			if (result.contains(currentSum)) {
+				return true;
+			}
+			result.add(currentSum);
+		}
+
+		return false;
+
+	}
+
+}
